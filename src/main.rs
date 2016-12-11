39adb6d6aaea3ae3771420a8225abb98de6a7a19
Radu Popescu <mail@radupopescu.net>
2016-12-11 20:04:10 +0100

    Use futures-rs and futures-cpupool to parse file in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-extern crate wikidata;
+extern crate futures;
+extern crate futures_cpupool;
 extern crate time;
+extern crate wikidata;
 
+use std::sync::mpsc::channel;
+
+use futures::Future;
+use futures_cpupool::CpuPool;
 use time::precise_time_ns;
 
 use wikidata::parse::parse_item;
@@ -14,14 +20,28 @@ fn main() {
     println!("Input file: {}", input_file);
     println!("Languages: {:?}", languages);
 
-    let mut elements = Vec::new();
+    let (tx, rx) = channel();
     if let Ok(streamer) = Streamer::new(&input_file) {
         let t00 = precise_time_ns();
+        let pool = CpuPool::new(4);
+        let mut futs = Vec::new();
         for line in streamer {
-            parse_item(&line, &languages)
-                .ok()
-                .and_then(|l| l) // flatten Option<Option<...> => Option<...>
-                .map(|e| {elements.push(e) });
+            let l = line.clone();
+            let langs = languages.clone();
+            let tx = tx.clone();
+            let t = pool.spawn_fn(move || {
+                parse_item(&l, &langs)
+                    .ok()
+                    .and_then(|l| l)
+                    .map(|e| {
+                        let _ = tx.send(e);
+                    });
+                Ok::<(), ()>(())
+            });
+            futs.push(t);
+        }
+        for f in futs {
+            let _ = f.wait();
         }
         let t10 = precise_time_ns();
         println!("Main loop: {} us", (t10 - t00) / 1000 );
@@ -30,7 +50,8 @@ fn main() {
     let t000 = precise_time_ns();
     let mut same = 0;
     let mut different = 0;
-    for e in elements {
+    drop(tx); // Close the original sender, since we won't be sending anything from it
+    for e in rx {
         if e[&languages[0]] == e[&languages[1]] {
             same += 1
         } else {
