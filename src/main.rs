ba596cefd730c03d19c0d572d06ed4f4fc561a5c
Radu Popescu <mail@radupopescu.net>
2017-03-11 11:34:34 +0100

    Produce and process parse results concurrently

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate futures_cpupool;
 extern crate time;
 extern crate wikidata;
 
+use std::thread;
+use std::sync::mpsc::{channel};
+
 use futures::Future;
 use futures_cpupool::CpuPool;
 use time::precise_time_ns;
@@ -20,30 +23,36 @@ fn main() {
     println!("Number of threads: {}", threads);
 
     let pool = CpuPool::new(threads);
-    let mut futs = Vec::new();
+    let (tx, rx) = channel();
     if let Ok(streamer) = Streamer::new(&input_file) {
-        let t00 = precise_time_ns();
-        for line in streamer {
-            let l = line.clone();
-            let langs = languages.clone();
-            futs.push(pool.spawn_fn(move || parse_item(&l, &langs)));
-        }
-        let t10 = precise_time_ns();
-        println!("Main loop: {} us", (t10 - t00) / 1000);
+        let langs = languages.clone(); // clone of langs to be moved into the other thread
+        thread::spawn(move || {
+            let tx = tx.clone();
+            let t00 = precise_time_ns();
+            for line in streamer {
+                let l = line.clone();
+                let langs = langs.clone(); // a new clone of langs for each future. Inefficient
+                let _ = tx.send(pool.spawn_fn(move || parse_item(&l, &langs)));
+            }
+            let t10 = precise_time_ns();
+            println!("Main loop: {} us", (t10 - t00) / 1000);
+        });
     }
 
     let t000 = precise_time_ns();
     let mut same = 0;
     let mut different = 0;
-    for f in futs {
-        f.wait().ok().and_then(|v| v).map(|v| {
+
+    for v in rx {
+        if let Some(v) = v.wait().ok().and_then(|v| v) {
             if v[&languages[0]] == v[&languages[1]] {
                 same += 1
             } else {
                 different += 1
             };
-        });
+        }
     }
+
     let t100 = precise_time_ns();
     println!("Counting: {} us", (t100 - t000) / 1000);
 
